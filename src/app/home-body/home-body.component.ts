86527d78da571847ad58358131283738610a3ee8
Heet Doshi <doshiheet2872003@gmail.com>
2023-12-16 16:06:48 +0530

    added files

--- src/app/home-body/home-body.component.ts
+++ src/app/home-body/home-body.component.ts
@@ -5,10 +5,12 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <p>
-      home-body works!
-    </p>
-  `,
+  <section class="listing">
+    <img class="listing-photo">
+    <h2 class="listing-heading"></h2>
+    <p class="listing-location"></p>
+</section>
+`,
   styleUrl: './home-body.component.css'
 })
 export class HomeBodyComponent {
---
+++ src/app/housing-location.ts
@@ -0,0 +1,5 @@
+export interface HousingLocation {
+    name: string,
+    id: number,
+
+}
