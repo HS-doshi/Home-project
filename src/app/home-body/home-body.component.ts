5fdb5f155a20f50c7da10904c6de6a15b44d1f90
Heet Doshi <doshiheet2872003@gmail.com>
2023-12-16 14:44:19 +0530

    Initial commit

---
+++ src/app/home-body/home-body.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-body',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      home-body works!
+    </p>
+  `,
+  styleUrl: './home-body.component.css'
+})
+export class HomeBodyComponent {
+
+}
